39fc85f0b6003923f27bf452e4b6833b032d9807
J. Austin Hughey <jaustinhughey@gmail.com>
2016-04-24 17:55:16 -0500

    Abstract VERSION, HOMEPAGE, SUMMARY, DESCRIPTION etc. - all needed for a `gemspec` - into `lib/ginsu/meta.rb`.

---
+++ ginsu.gemspec
@@ -0,0 +1,36 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'ginsu/meta' # includes version info
+
+Gem::Specification.new do |spec|
+  spec.name             =       "ginsu"
+  spec.version          =       Ginsu::Meta::VERSION
+  spec.authors          =       ["J. Austin Hughey"]
+  spec.email            =       ["jaustinhughey@gmail.com"]
+  spec.description      =       Ginsu::Meta::DESCRIPTION
+  spec.summary          =       Ginsu::Meta::SUMMARY
+  spec.homepage         =       Ginsu::Meta::HOMEPAGE
+  spec.license          =       Ginsu::Meta::LICENSE
+
+  if spec.respond_to?(:metadata)
+    spec.metadata['allowed_push_host'] = "Delete this to push to rubygems.org"
+  else
+    raise "RubyGems 2.0 or newer is necessary. Upgrade to continue."
+  end
+
+  spec.files = `git ls-files -z`.split("\x0").reject do |f|
+    f.match(%r{^(test|spec|features)/})
+  end
+
+  spec.bindir = "exe"
+
+  spec.executables = spec.files.grep(%r{^exe/}) do |f|
+    File.basename(f)
+  end
+
+  spec.require_paths = ['lib']
+
+  spec.add_development_dependency "bundler", "~> 1.11"
+  spec.add_development_dependency "rake", "~> 10.0"
+end
---
+++ lib/ginsu/meta.rb
@@ -0,0 +1,112 @@
+class Ginsu
+  module Meta
+    #
+    # VERSION sets the application's current version. Definitely trying to go
+    # for a simplified [Semantic Versioning](http://semver.org) scheme here,
+    # where A.B.C would be the pattern:
+    #
+    #   A -> Major release version; breaks backwards compatibility. New features
+    #        and/or other major changes.
+    #   B -> Minor release version; new features, but it *DOES NOT* break
+    #        backwards compatibility. Everything that worked before should
+    #        still work without any changes. If not, it's a bug.
+    #   C -> Patch release version; bug fixes only. No new features, and
+    #        everything is still backwards compatible.
+    #
+    # So for example:
+    #
+    #   0.0.1: pre-alpha/dev version. Say I find a bug, then I fix it.
+    #   0.0.2: exact same as 0.0.1 except I fixed the bug. Yay!
+    #   0.1.0: I got bored so I built a new feature! Fully backward-compatible.
+    #   0.1.1: Found a bug with version 0.1.0 so I fixed it and pushed a fully
+    #          backwards-compatible release.
+    #   0.1.2: Found and fixed another bug. Everything that worked with 0.1.0,
+    #          and 0.1.1 should still work without any modification with this
+    #          new version. Just a bug fix.
+    #   0.1.3: Found and fixed yet another bug! Works exactly the same as 0.1.0,
+    #          0.1.1, and 0.1.2.
+    #   1.0.0: FIRST OFFICIAL RELEASE, BABY! Since the MAJOR version number
+    #          changed, **you should NOT expect this version to work like the
+    #          previous ones you've used**. Major new features may have been
+    #          implemented, the internals may have been rewritten, and/or other
+    #          changes to workflow, the tool's capabilities, etc. may have
+    #          happened so if something breaks, well, you knew it was a MAJOR
+    #          release! You therefore have only yourself to blame. :)
+    #
+    VERSION = '0.0.1'
+
+    #
+    #
+    # HOMEPAGE specifies where the repo lives on the wonderful wonderful web! :)
+    #
+    HOMEPAGE = "https://github.com/insurrectiondigital/ginsu"
+
+    #
+    # A written, short summary of what this tool does.
+    #
+    SUMMARY = "Gigantic file? Slice it into pieces with ginsu! (Also restores.)"
+
+    #
+    # A much longer and more detailed description of how `ginsu` works.
+    #
+    DESCRIPTION <<-EOF
+      # Files too big? Slice 'em up with `ginsu`!
+
+      Say you have a huge backup archive that you want to upload to some
+      online storage provider:
+
+      ```
+      $ ls -Flach myarchive.tar.bz2
+      -rw-r--r--@  1 you  staff   72G Apr 24 04:04 myarchive.tar.bz2
+      ```
+
+      Only problem: your storage provider might whine that they won't accept
+      uploaded files larger than `N` gigabytes in size. Bummer! What's a
+      hacker/sysadmin to do?
+
+      ## Slice it up with `ginsu`!
+
+      `ginsu` will take a given source file (say your 72GB .tar.bz2) and
+      slice it up into smaller chunks, the size of which you specify.
+
+      In a nutshell, you use it like this:
+
+      ```
+      $ ginsu slice myarchive.tar.bz2 /Volumes/Backups/myarchive 250MB
+      ```
+
+      Where:
+
+      + `slice` tells `ginsu` that you want to cut up a file, as opposed to
+      merging it back together (a "restore", of sorts);
+      + `myarchive.tar.bz2` is the path on local filesystem to your huge
+      unwieldly archive;
+      + `/Volumes/Backups/myarchive` is where you want the sliced up files
+      to be stored. **Directory will be created if it does not exist**, and
+      under that directory will be a new directory named, in this example,
+      `myarchive.tar.bz2.ginsu/`. It's basically the filename plus `.ginsu`.
+      Under that directory is where all the various slices will be stored
+      with filenames reflecting the original checksum of the original archive
+      appended with their sequence in reassembly order and `.slice` so you
+      know what kind of file it is.
+      + `250MB` is how big you want each resulting slice to be. `ginsu` will
+      do the math for you on how many slices you'll need and loop through
+      cutting each one until the task is complete, updating you along the way.
+
+      Restoring is also a piece of cake. For example:
+
+      ```
+      $ ginsu join /Volumes/Backups/myarchive/myarchive.tar.bz2.ginsu \
+      $HOME/i/want/the/original/file/put/here
+      ```
+
+      This would bring together the various "slices" of your original file and
+      put the resulting file, with its original filename, at the path specified.
+
+      ## Interested yet?
+
+      Yeah, I thought so. Head on over to #{Ginsu::Meta::HOMEPAGE} to read
+      more about this tool.
+    EOF
+  end
+end
