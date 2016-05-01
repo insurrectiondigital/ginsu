e6f181b174f73bd9677e263ca5dcebd2c333047d
J. Austin Hughey <jaustinhughey@gmail.com>
2016-05-01 02:24:01 -0500

    Test skeletons
    Basic skeletons for CLI, the "split" command, and a generic filesystem (is this file there? can I write to it? etc.) test

---
+++ test/functional/split_test.rb
@@ -0,0 +1,12 @@
+require_relative '../test_helper'
+
+#
+# test_split
+#
+# Tests all the major functionality related to the `ginsu split` command.
+#
+class TestGinsuSplit < Minitest::Test
+  def test_splits_a_file
+    skip 'TODO'
+  end
+end
